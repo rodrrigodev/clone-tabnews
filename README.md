2b8e0d0292f38f278da2bbebd0a8afc62f333cf2
Rodrigo <101267761+rodrrigodev@users.noreply.github.com>
2023-10-10 13:27:42 +0000

    add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto criado para desenvolvimento(clone) da plataforma tabnews no curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-export default function Home(){
-    return(
-        <h1>Você pode aprender um segundo idioma se Você quiser! Basta Você se esforçar e ter pasciência.</h1>
-    )
+export default function Home() {
+  return (
+    <h1>
+      Você pode aprender um segundo idioma se Você quiser! Basta Você se
+      esforçar e ter pasciência.
+    </h1>
+  );
 }
